940aeeb57e345c10d1d58c7e0c70b72517106678
LyubovNesterchuk <nesterchuklyubov@gmail.com>
2025-09-29 14:52:34 +0300

    all

--- src/routes/notesRoutes.js
+++ src/routes/notesRoutes.js
@@ -1,12 +1,12 @@
 import { Router } from "express";
 import { celebrate } from "celebrate";
 
-import { getNotes, getNoteById, createNote, updateNote, deleteNote } from "../controllers/notesController.js";
+import { getAllNotes, getNoteById, createNote, updateNote, deleteNote } from "../controllers/notesController.js";
 import { getAllNotesSchema, noteIdSchema, createNoteSchema, updateNoteSchema } from "../validations/notesValidation.js";
 
 const router = Router();
 
-router.get("/notes", celebrate(getAllNotesSchema), getNotes);
+router.get("/notes", celebrate(getAllNotesSchema), getAllNotes);
 router.get("/notes/:noteId", celebrate(noteIdSchema), getNoteById);
 router.post("/notes", celebrate(createNoteSchema), createNote);
 router.patch("/notes/:noteId", celebrate(updateNoteSchema), updateNote);
