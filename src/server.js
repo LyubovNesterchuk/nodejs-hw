f806c9feca7a466e8bbb89b38cde9551d80abd52
LyubovNesterchuk <nesterchuklyubov@gmail.com>
2025-11-27 15:35:27 +0200

    cors

--- src/server.js
+++ src/server.js
@@ -15,11 +15,26 @@ import userRoutes from "./routes/userRoutes.js";
 
 
 const app = express();
+
 const PORT = process.env.PORT ?? 3030;
+const allowedOrigins = [
+  'http://localhost:3030',
+  'https://09-auth-ochre-kappa.vercel.app/api',
+];
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to NoteHub API' });
+});
 
 app.use(logger);
+
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true,
+  }),
+);
+
 app.use(cookieParser());
 
 app.use(authRoutes);
