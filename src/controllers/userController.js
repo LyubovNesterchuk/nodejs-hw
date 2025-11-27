9ef1d123f72532c40d19fcb8753ff69d931493fb
LyubovNesterchuk <nesterchuklyubov@gmail.com>
2025-11-27 21:13:18 +0200

    getMe, updateMe controller

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,6 +2,40 @@ import createHttpError from 'http-errors';
 import { User } from '../models/user.js';
 import { saveFileToCloudinary } from '../utils/saveFileToCloudinary.js';
 
+
+export const getMe = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.user._id);
+
+    if (!user) {
+      throw createHttpError(404, "User not found");
+    }
+
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const updateMe = async (req, res, next) => {
+  try {
+    const updates = {
+      username: req.body.username,
+      email: req.body.email,
+      avatar: req.body.avatar,
+    };
+
+    const user = await User.findByIdAndUpdate(req.user._id, updates, {
+      new: true,
+    });
+
+    res.json(user);
+  } catch (err) {
+    next(err);
+  }
+};
+
+
 export const updateUserAvatar = async (req, res, next) => {
   if (!req.file) {
     next(createHttpError(400, 'No file'));
--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import { authenticate } from '../middleware/authenticate.js';
 import { upload } from "../middleware/multer.js";
-import { updateUserAvatar } from '../controllers/userController.js';
+import { getMe, updateMe, updateUserAvatar } from "../controllers/userController.js";
 
 const router = Router();
 
+router.get("/users/me", authenticate, getMe);
+router.patch("/users/me", authenticate, updateMe);
 router.patch("/users/me/avatar", authenticate, upload.single("avatar"), updateUserAvatar);
 
 export default router;
