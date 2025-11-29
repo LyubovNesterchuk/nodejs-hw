01c6ecef08fa0a8dbc93d0f40485db10771f7812
LyubovNesterchuk <nesterchuklyubov@gmail.com>
2025-11-29 17:30:17 +0200

    fix

--- src/server.js
+++ src/server.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 import { errors } from "celebrate";
 import cookieParser from "cookie-parser";
 import { connectMongoDB } from "./db/connectMongoDB.js";
-import helmet from 'helmet';
+// import helmet from 'helmet';
 
 import { logger } from "./middleware/logger.js";
 import { notFoundHandler } from "./middleware/notFoundHandler.js";
@@ -17,42 +17,26 @@ import userRoutes from "./routes/userRoutes.js";
 const app = express();
 
 const PORT = process.env.PORT ?? 3030;
-
+const allowedOrigins = [
+  'http://localhost:3000',
+  'https://note-hub-alpha.vercel.app',
+];
 
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Welcome to NoteHub API' });
 });
 
 app.use(logger);
-app.use(helmet());
+// app.use(helmet());
 
 app.use(express.json());
-// app.use(
-//   cors({
-//     origin: allowedOrigins,
-//     credentials: true,
-//     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-//     allowedHeaders: ['Content-Type', 'Authorization'],
-//   }),
-// );
 app.use(
   cors({
+    origin: allowedOrigins,
     credentials: true,
-    origin: (origin, callback) => {
-      if (!origin) return callback(null, true);
-
-      const allowedOrigins = [
-        "http://localhost:3000",
-        "https://note-hub-alpha.vercel.app",
-      ];
-
-      if (allowedOrigins.includes(origin)) {
-        return callback(null, true);
-      }
-
-      return callback(new Error("CORS blocked: " + origin), false);
-    },
-  })
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  }),
 );
 
 app.use(cookieParser());
